6e7136f37688ed792a756328f818ab049088667b
1114209086 <1114209086@qq.com>
2017-05-12 00:49:22 +0800

    add log config and use

--- src/main/java/com/bill/firstapplication/controller/impl/MyController.java
+++ src/main/java/com/bill/firstapplication/controller/impl/MyController.java
@@ -5,16 +5,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 
 import com.bill.firstapplication.modol.User;
 
 @Controller
 public class MyController {
-
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	
 	@RequestMapping("/username")
 	@ResponseBody
 	public String getUserName(){
+		log.info("getUserName", "in");
 		return "Bill";
 	}
 	@RequestMapping("/user")
