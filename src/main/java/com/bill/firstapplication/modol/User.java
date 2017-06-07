57197d0539b63babded8fefe7776f97323627dbe
1114209086 <1114209086@qq.com>
2017-06-07 23:34:10 +0800

    entry class mapping and add has/ove

--- src/main/java/com/bill/firstapplication/modol/User.java
+++ src/main/java/com/bill/firstapplication/modol/User.java
@@ -1,13 +1,26 @@
 package com.bill.firstapplication.modol;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 
+	@Id@GeneratedValue
 	private Integer identity;
+	@Column(nullable = false)
 	private String name;
+	@Column
 	private String age;
+	@Column
 	private String sex;
+	@Column
 	private String email;
+	@Column
 	private String phoneNumber;
+	@Column
 	private String address;
 	public Integer getIdentity() {
 		return identity;
@@ -50,6 +63,34 @@ public class User {
 	}
 	public void setAddress(String address) {
 		this.address = address;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((identity == null) ? 0 : identity.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (identity == null) {
+			if (other.identity != null)
+				return false;
+		} else if (!identity.equals(other.identity))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "User [identity=" + identity + ", name=" + name + ", age=" + age + ", sex=" + sex + ", email=" + email
+				+ ", phoneNumber=" + phoneNumber + ", address=" + address + "]";
 	}
 	
 }
