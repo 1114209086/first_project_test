b3c67968257fdc3f097f0580eef0af9978a6f86b
1114209086 <1114209086@qq.com>
2017-06-12 23:34:31 +0800

    add Custom Queries

--- src/main/java/com/bill/firstapplication/dao/impl/UserDao.java
+++ src/main/java/com/bill/firstapplication/dao/impl/UserDao.java
@@ -1,6 +1,8 @@
 package com.bill.firstapplication.dao.impl;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.bill.firstapplication.modol.User;
 import java.lang.String;
@@ -9,4 +11,7 @@ import java.util.List;
 public interface UserDao extends CrudRepository<User, Integer>{
 	
 	List<User> findByAccount(String account);
+	
+	@Query("select u from User u where u.address=:address and u.phoneNumber=:phoneNumber")
+	List<User> findByAddressAndPhoneNumber(@Param("address") String address,@Param("phoneNumber") String phoneNumber);
 }
