51fe1129d5d4f883d60d16cee9ce61de8117a8af
1114209086 <1114209086@qq.com>
2017-06-14 00:24:24 +0800

    success save user to h2 database

--- src/main/java/com/bill/firstapplication/controller/impl/RegistrationController.java
+++ src/main/java/com/bill/firstapplication/controller/impl/RegistrationController.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bill.firstapplication.controller.BaseController;
 import com.bill.firstapplication.modol.User;
+import com.bill.firstapplication.service.impl.UserService;
 
 @Controller
 public class RegistrationController implements BaseController{
 
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 	
+	@Autowired
+	UserService service;
 	
 	@RequestMapping(value = "/registration", method = RequestMethod.GET )
 	public String registrationPage(){
@@ -24,8 +26,11 @@ public class RegistrationController implements BaseController{
 	}
 	@RequestMapping(value = "/registration", method = RequestMethod.POST)
 	public String register(User user, Model model){
-		model.addAttribute("user",user);
-		logger.info(user.toString());
-		return "welcome";
+		if (service.addUser(user).equals("success")) {
+			model.addAttribute("user",user);
+			logger.info(user.toString());
+			return "welcome";
+		}
+		return "error";
 	}
 }
