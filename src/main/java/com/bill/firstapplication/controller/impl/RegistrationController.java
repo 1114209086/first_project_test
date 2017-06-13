ab059fabe7218a747466fa5596b4734e36a53ccd
1114209086 <1114209086@qq.com>
2017-06-14 00:14:47 +0800

    mapping register inf to object

--- src/main/java/com/bill/firstapplication/controller/impl/RegistrationController.java
+++ src/main/java/com/bill/firstapplication/controller/impl/RegistrationController.java
@@ -1,16 +1,31 @@
 package com.bill.firstapplication.controller.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bill.firstapplication.controller.BaseController;
+import com.bill.firstapplication.modol.User;
 
 @Controller
 public class RegistrationController implements BaseController{
 
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	
 	@RequestMapping(value = "/registration", method = RequestMethod.GET )
 	public String registrationPage(){
 		return "registration";
+	}
+	@RequestMapping(value = "/registration", method = RequestMethod.POST)
+	public String register(User user, Model model){
+		model.addAttribute("user",user);
+		logger.info(user.toString());
+		return "welcome";
 	}
 }
